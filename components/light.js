d4c5bc83cabc082951143a993d211f727cd6cb16
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2021-11-03 15:02:11 -0500

    Modularised

--- components/light.js
+++ components/light.js
@@ -5,7 +5,6 @@ import {
 } from "three";
 
 function createLight(type, options = {}) {
-  let light = new DirectionalLight(color, intensity);
   const {
     color = "white",
     intensity = 1,
@@ -14,6 +13,7 @@ function createLight(type, options = {}) {
     height = 10,
     decay = null,
   } = options;
+  let light = new DirectionalLight(color, intensity);
   if (type == "dir") light = new DirectionalLight(color, intensity);
   if (type == "point") light = new PointLight(color, intensity, distance, decay);
   if (type == "rect") light = new RectAreaLight(color, intensity, width, height);
